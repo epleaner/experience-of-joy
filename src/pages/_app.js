7001dfee012e52e7f51d6355bd86b6882a31d98b
Eli <eliipleaner@gmail.com>
2020-08-05 15:14:30 +0200

    Add enter/exit page transitions

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,14 @@
 import '@styles/styles.css';
+import { PageTransition } from 'next-page-transitions';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps, router }) {
+  return (
+    <>
+      <PageTransition timeout={1500} classNames='h-full w-full page-transition'>
+        <Component {...pageProps} key={router.route} />
+      </PageTransition>
+    </>
+  );
 }
 
 export default MyApp;
