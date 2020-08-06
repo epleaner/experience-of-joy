720e29cb0a9e5d53755dc69dc7a0d973b8423eef
Eli <eliipleaner@gmail.com>
2020-08-06 13:09:40 +0200

    Add react transition group for experience list

--- src/components/ExperiencesList.js
+++ src/components/ExperiencesList.js
@@ -1,13 +1,21 @@
 import PropTypes from 'prop-types';
 
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+
 const ExperiencesList = ({ experiences }) => {
-  return experiences
-    .sort((a, b) => a.date < b.date)
-    .map(({ _id, experience }) => (
-      <p key={_id} className='m-8 my-10 md:my-16 text-2xl md:text-4xl'>
-        {experience}
-      </p>
-    ));
+  return (
+    <TransitionGroup>
+      {experiences
+        .sort((a, b) => a.date < b.date)
+        .map(({ _id, experience }) => (
+          <CSSTransition key={_id} timeout={500} classNames='item'>
+            <p className='m-8 my-10 md:my-16 text-2xl md:text-4xl'>
+              {experience}
+            </p>
+          </CSSTransition>
+        ))}
+    </TransitionGroup>
+  );
 };
 
 ExperiencesList.propTypes = {
